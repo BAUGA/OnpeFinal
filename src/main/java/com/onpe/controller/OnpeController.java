cd5ec33f1d333f46838d113b9692db1a35bec87b
BAUYES <BAUYES@DESKTOP-97FOU1L>
2023-04-17 16:53:53 -0500

    Actualizando

--- src/main/java/com/onpe/controller/OnpeController.java
+++ src/main/java/com/onpe/controller/OnpeController.java
@@ -1,11 +1,16 @@
 package com.onpe.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.onpe.service.IGrupoCasoService;
 
 
 @Configuration
@@ -15,7 +20,6 @@ public class OnpeController  extends WebMvcAutoConfiguration{
 	public void addCorsMappings (CorsRegistry registry) {
 		registry.addMapping("/**");
 	}
-		
 	@GetMapping ("/index")
 
 	public String index() {
@@ -23,8 +27,36 @@ public class OnpeController  extends WebMvcAutoConfiguration{
 		return "index";
 	}
 
+	@GetMapping ("/actas")
 
+	public String actas() {
+		
+		return "actas";
+	}
+
+	@GetMapping ("/actas_i")
+
+	public String actas_info() {
+		
+		return "actas_info";
 	}
 
 
+	@GetMapping ("/GeneralP")
 
+	public String GeneralPresi() {
+		
+		return "GeneralPrecidencial";
+	}
+
+	@Autowired 
+	private IGrupoCasoService iGrupoCasoService; 
+
+	@GetMapping ("/actas_info/{id}")
+	public String listar (Model modelo, @PathVariable ("id")String id) {
+		
+	modelo.addAttribute("actas", iGrupoCasoService.getGrupoVotacion(id));
+	return "actas_info";
+	}
+
+	}
