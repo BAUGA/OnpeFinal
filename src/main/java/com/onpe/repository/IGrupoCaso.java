b30626b23fb6f1fd4636d514d6f6a8d65b23b83f
BAUYES <BAUYES@DESKTOP-97FOU1L>
2023-04-17 15:57:34 -0500

    Actualizando

--- src/main/java/com/onpe/repository/IGrupoCaso.java
+++ src/main/java/com/onpe/repository/IGrupoCaso.java
@@ -1,5 +1,15 @@
 package com.onpe.repository;
 
-public class IGrupoCaso {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.onpe.Model.GrupoCaso;
+
+@Repository
+public interface IGrupoCaso extends JpaRepository <GrupoCaso,String> {
+	@Query (value= "{call usp_getGrupoVotacion(:id)}",nativeQuery=true)
+	GrupoCaso getGrupoVotacion(String id);
+	
 
 }
